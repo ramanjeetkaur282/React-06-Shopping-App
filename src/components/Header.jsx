adcaa78f831aa05350397b95c0877ed3370e9882
ramanjeetkaur282 <54283512+ramanjeetkaur282@users.noreply.github.com>
2024-07-04 12:13:02 -0400

    Update Header.jsx
    Updated Name Again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
       <header id="main-header">
         <div id="main-title">
           <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Clothes</h1>
+          <h1>Elegant Attire Checkout</h1>
         </div>
         <p>
           <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
